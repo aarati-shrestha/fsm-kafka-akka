8f825504c576b8f0c75fafab640805929578f8c2
aarati-shrestha <aarati@fusemachines.com>
2016-09-26 16:20:34 +0545

    added filter

--- src/main/java/com/demo/akka/FiniteStateMachineConsumerActor.java
+++ src/main/java/com/demo/akka/FiniteStateMachineConsumerActor.java
@@ -14,10 +14,14 @@ import kafka.utils.ZKStringSerializer$;
 
 import org.I0Itec.zkclient.ZkClient;
 import org.I0Itec.zkclient.ZkConnection;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.json.JsonParser;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
@@ -38,56 +42,59 @@ public class FiniteStateMachineConsumerActor extends UntypedActor {
 	
 	@Autowired	
 	private FSMBase fsmBase;
-	
-	ZkClient zkClient = null;
-    ZkUtils zkUtils = null;
-    SimpleConsumer simpleConsumer = new SimpleConsumer("localhost:2181", "coregroup");
+	private SimpleConsumer sc = null;
+	private ConsumerConnector consumerConnector = null;
 
 	@Override
 	public void onReceive(Object msg) throws Exception {
 		if(msg.toString().equals("ACTIVE")){
 			System.out.println("inside active");
-			zkClient = new ZkClient("localhost:2181", 10000, 10000, ZKStringSerializer$.MODULE$);
-			Properties props = new Properties();
-			zkUtils = new ZkUtils(zkClient, new ZkConnection("localhost:2181"), false);
-			if(!AdminUtils.topicExists(zkUtils, topic)){
-				System.out.println("topic created");
-				AdminUtils.createTopic(zkUtils, topic, 10, 1, props, null);
-				SimpleConsumer sc = new SimpleConsumer("localhost:2181", "coregroup");
-		        ConsumerConnector consumerConnector = sc.getConsumerConnector();
-		        logger.info("Running.....");
-		        Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
-		        topicCountMap.put(topic, new Integer(1));
-		        Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumerConnector
-		                .createMessageStreams(topicCountMap);
-		        KafkaStream<byte[], byte[]> stream = consumerMap.get(topic).get(0);//remove args if error
-		        ConsumerIterator<byte[], byte[]> it = stream.iterator();
-		        while (it.hasNext()) {
-		            String newString = new String(it.next().message());
-		            logger.info("newString :: {}",newString);
-		            processMessage(newString);	
-		            consumerConnector.shutdown();
+			sc = new SimpleConsumer("localhost:2181", "coregroup");
+	        consumerConnector = sc.getConsumerConnector();
+	        logger.info("Running.....");
+	        Map<String, Integer> topicCountMap = new HashMap<String, Integer>();
+	        topicCountMap.put(topic, new Integer(1));
+	        Map<String, List<KafkaStream<byte[], byte[]>>> consumerMap = consumerConnector
+	                .createMessageStreams(topicCountMap);
+	        KafkaStream<byte[], byte[]> stream = consumerMap.get(topic).get(0);//remove args if error
+	        ConsumerIterator<byte[], byte[]> it = stream.iterator();
+	        while (it.hasNext()) {
+	        	if(fsmBase.getState().equals(State.IDLE)){
+	            	consumerConnector.shutdown();
 		            break;
-		        }
-			}				
-		}	
-		else{
-			System.out.println("inside idle");
-			String zookeeperHosts = "localhost:2181";		
-			zkClient = new ZkClient("localhost:2181", 10000, 10000, ZKStringSerializer$.MODULE$);
-			zkUtils = new ZkUtils(zkClient, new ZkConnection(zookeeperHosts), false);
-			if(AdminUtils.topicExists(zkUtils, topic)){
-				AdminUtils.deleteTopic(zkUtils, topic);	
-				System.out.println("topic deleted");
-			}
-		}								
+	            }
+	            String newString = new String(it.next().message());
+	            logger.info("newString :: {}",newString);
+	            processMessage(newString);	
+	            
+	            
+	        }							
+		}									
 	}
 	
 	
-	public void processMessage(String newString){	
-		//System.out.println("execute once");
-    	System.out.println(newString);      
-    	fsmBase.setState(State.IDLE);
-}
+	public void processMessage(String newString){
+		JSONObject jsonObj = this.isJSONValid(newString);
+		if(jsonObj != null){
+			System.out.println(newString);
+			fsmBase.setState(State.IDLE);
+		}
+    	 
+    	
+	}
+	
+	
+	public JSONObject isJSONValid(String test) {
+		JSONObject jsonObj = null;
+	    try {
+	       jsonObj =  new JSONObject(test);
+	       if(jsonObj.has("randomId")&& jsonObj.get("randomId").toString().equalsIgnoreCase("12345")){
+	    	   return jsonObj;
+	       }
+	    } catch (JSONException ex) {	        
+	       
+	    }
+	    return null;
+	}
 
 }
